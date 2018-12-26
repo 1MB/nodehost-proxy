61e83ef93eb6672b31faf05f913ee9f62451b8b3
Anthony Rossbach <me@anthonys.space>
2018-12-26 15:39:57 -0800

    Added new cache system

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Startup Rules
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+error_reporting(0);
+ini_set('display_errors', 0);
 
-//Function that when given a domain will validate if it has a SSL certificate
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Credits
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// Maintained for 1mbsite by Anthony Rossbach
+
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Future stuff
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//API for resources api.1mb.site/?action=resources&site=dalton
+
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Functions
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 function has_ssl($domain) {
+	//Function that when given a domain will validate if it has a SSL certificate
 	$res = false;
 	$orignal_parse = $domain;
 	$stream = @stream_context_create( array( 'ssl' => array( 'capture_peer_cert' => true ) ) );
@@ -24,19 +44,11 @@ function has_ssl($domain) {
 	return $res;
 }
 
-//Auto redirect to HTTPS first if not already using HTTPS
-//AKA If the domain we are on now has a valid certificate we should send the user to the HTTPS version of our site...
-if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
-	if (has_ssl($_SERVER['HTTP_HOST'])==true){
-		//Only redirect if we have a valid ssl cert first
-		$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-		header('HTTP/1.1 301 Moved Permanently');
-		header('Location: ' . $redirect);
-		exit();
-	}
-}
-
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Custom URL content
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 if (isset($_GET["rules"])){
+	//Custom rules selector for 1mbsite panel
 	header("Content-Type: text/plain");
 	if ($_GET["rules"]=="ssl"){
 		if (has_ssl($_SERVER['HTTP_HOST'])==true){
@@ -48,8 +60,11 @@ if (isset($_GET["rules"])){
 	exit();
 }
 
-//If the username is not present then give the user a message, else save and redirect to site root
-if (!file_exists("username.txt")) {
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Check for username file
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+if (!file_exists("username.txt")){
+	//If the username is not present then give the user a message, else save and redirect to site root
 	$username = !empty($_GET["username"]) ? strtolower($_GET["username"]) : false;
 	if ($username === false) {
 		echo "<html><head></head><body><h1 style='text-align:center;margin-top:150px;'>This custom domain is not setup, click the link on the 1MB dashboard to finish setup.</h1></body></html>";
@@ -62,17 +77,41 @@ if (!file_exists("username.txt")) {
 	}
 }
 
-//Build page URL
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Build links and settings
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 $link_request=$_SERVER[REQUEST_URI];
 if ($link_request=="/"){
 	$link_request="";
 }
-
-//Remove www. from our request to the backend server.
-$mydomain = str_replace("www.", "", $_SERVER['HTTP_HOST']);
+$mydomain=str_replace("www.", "", $_SERVER['HTTP_HOST']);
 $username=file_get_contents("username.txt");
+$cachecode=sha1($_SERVER[REQUEST_URI]);
+$httpprefix="http://";
+$source=''.$username.'.1mb.site';
+$usecache=false;
+$url = 'https://'.$username.'.1mb.site' . $link_request;
 
-//Redirect if we are using www.
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ HTTPS redirect
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//Auto redirect to HTTPS first if not already using HTTPS
+//AKA If the domain we are on now has a valid certificate we should send the user to the HTTPS version of our site...
+if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
+	if (has_ssl($_SERVER['HTTP_HOST'])==true){
+		//Only redirect if we have a valid ssl cert first
+		$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		header('HTTP/1.1 301 Moved Permanently');
+		header('Location: ' . $redirect);
+		exit();
+	}
+}else{
+	$httpprefix="https://";
+}
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Redirect away from www
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 if ($mydomain!=$_SERVER['HTTP_HOST']){
 	$redirect = 'https://' . $mydomain . $_SERVER['REQUEST_URI'];
 	header('HTTP/1.1 301 Moved Permanently');
@@ -80,39 +119,115 @@ if ($mydomain!=$_SERVER['HTTP_HOST']){
 	exit();
 }
 
-//The URL to request
-$url = 'https://'.$username.'.1mb.site' . $link_request;
-
-//Send request, with the current visitors useragent
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-//If this reuqest is a post request forward on the POST data
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Check for cache version first
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+if (file_exists("cache_static_".$cachecode."_timestamp.txt")){
+	$cachetime=file_get_contents("cache_static_".$cachecode."_timestamp.txt");
+	$checktime=(time()-(60*30));
+	if ($checktime<=$cachetime){
+		//Cache is good to use
+		$usecache=true;
+		$timeleft=$cachetime-$checktime;
+		header("1mbproxy-cache-time: ".$timeleft."");
+		header("1mbproxy-cache-archive: ".$cachetime."");
+		header("1mbproxy-cache-now: ".$checktime."");
+	}else{
+		//Nope cache expired
+		$usecache=false;
+		header("1mbproxy-cache-time: refresh");
+	}
 }
 
-//Run request and get response
-$response = curl_exec($ch);
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Main script starts
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-//Check if response failed, if so give the user a error message
-if (curl_error($ch)) {
-	echo "<html><head></head><body><h1 style='text-align:center;margin-top:150px;'>We are unable to run this requst</h1></body></html>";
-	exit();
+if ($usecache==false){
+	//######################################
+	//###################################### Load fresh version
+	//######################################
+
+	//Send request, with the current visitors useragent
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2); //Wait 2 seconds to connect
+	curl_setopt($ch, CURLOPT_TIMEOUT, 8); //If cant process in 8 seconds its a timeout, our connection is 1GBPS so no problem should happen
+
+	//If this reuqest is a post request forward on the POST data
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+	}
+
+	//Run request and get response
+	$response = curl_exec($ch);
+	$type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+	//Check type here and change URL to new one for links and source content!
+	if (strpos($type, 'text/html') !== false || strpos($type, 'text/css') !== false || strpos($type, 'application/javascript') !== false){
+		$response = str_replace("https://".$source, "//".$_SERVER['HTTP_HOST']."", $response);
+		$response = str_replace("http://".$source, "//".$_SERVER['HTTP_HOST']."", $response);
+	}
+
+	//Check if response failed, if so give the user a error message
+	if (curl_error($ch)){
+		if (!file_exists("cache_realtime_".$cachecode."_content.txt")){
+			//No cache kill page
+			header("1mbproxy-Cache: false");
+			header("Content-Type: text/html");
+			echo "<html><head></head><body><h1 style='text-align:center;margin-top:150px;'>We are unable to run this requst</h1></body></html>";
+			exit();
+		}else{
+			//We have a cache version load it!
+			header("1mbproxy-Cache: true");
+			$content=file_get_contents("cache_realtime_".$cachecode."_content.txt");
+			$type=file_get_contents("cache_realtime_".$cachecode."_type.txt");
+			header("Content-type: $type");
+			echo $content;
+			exit();
+		}
+	}else{
+		//No error we can save the resource
+		//Set browser content type for the file and send that along so it renders as it should
+		header("Content-type: $type");
+		if (!file_exists("cache_realtime_".$cachecode."_content.txt")){
+			header("1mbproxy-cache: saved");
+		}else{
+			header("1mbproxy-cache: updated");
+		}
+
+		//save realtime version first
+		file_put_contents("cache_realtime_".$cachecode."_content.txt", $response);
+		file_put_contents("cache_realtime_".$cachecode."_type.txt", $type);
+
+
+		//save static version
+		if (strpos($type, 'text/html') === false){
+			file_put_contents("cache_static_".$cachecode."_content.txt", $response);
+			file_put_contents("cache_static_".$cachecode."_type.txt", $type);
+			file_put_contents("cache_static_".$cachecode."_timestamp.txt", time());
+		}
+
+	}
+
+	//Close connection and paste response
+	curl_close($ch);
+	echo $response;
+
+}else{
+	//######################################
+	//###################################### Load cache version
+	//######################################
+	header("1mbproxy-cache: true");
+	$content=file_get_contents("cache_static_".$cachecode."_content.txt");
+	$type=file_get_contents("cache_static_".$cachecode."_type.txt");
+	header("Content-type: $type");
+	echo $content;
+
 }
-
-//Set browser content type for the file and send that along so it renders as it should
-$contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-header("Content-type: $contentType");
-
-//Close connection
-curl_close($ch);
-
-//Send the data to the browser and we are done
-echo $response;
 
 ?>
